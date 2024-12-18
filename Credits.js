153b8adc57396152a37c449bc21a050e2dc7074a
rmerin <102548532+rmerin@users.noreply.github.com>
2024-12-18 17:19:25 -0500

    Add files via upload

---
+++ Credits.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+const creditsAll {
+    developerName: "Ronald G.Merino",
+    profName: "Paul Engin",
+    course: "Game And Mobile Development",
+    college: "Finger Lakes Community College",
+    collegeAddress1: "3325 Marvin Sand Drive",
+    collegeAddress2: "Candaigua, NY 14424",
+    credits: function() 
+    {
+        return this.developerName + " " + this.profName + " " + this.course + " " + this.college + " " + this.collegeAddress1 + " " + this.collegeAddress2;
+    }
+}
